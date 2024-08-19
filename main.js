22c3550d822cb5a6790249e54d44b962392a0f8c
alainprof <amartel@cstj.qc.ca>
2024-08-19 11:31:13 -0400

    introduction d electron dans l'app

---
+++ main.js
@@ -0,0 +1,15 @@
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+function onReady () {
+  win = new BrowserWindow({width: 600, height: 300, frame:false })
+  win.loadURL(url.format({
+    pathname: path.join(
+    __dirname,
+    'dist/poker420/browser/index.html'),
+    protocol: 'file:',
+    slashes: true
+   })
+  )
+}
+app.on('ready', onReady);
